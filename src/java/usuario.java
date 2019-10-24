a3b8dd72118e0897eb314ef6f5a229f07cb03a10
Aluno19 <Aluno19@DESKTOP-LCSIKG5>
2019-10-24 10:32:20 -0300

    inicio do projeto

---
+++ src/java/usuario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aluno19
+ */
+public class usuario {
+   
+     static String Nome;
+      static String Email;
+       static String Senha;
+    
+    
+}
